9515447b5008e5956475c29fd7c16b0c35bf3ae4
unknown <haniehafzali639@gmail.com>
2025-03-09 13:02:23 +0330

    init project and install tailwind

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import Post from "../components/Post";
+
+export default function Home() {
+    return <h1 className="py-10 rounded-[14px] bg-[#F1DFDF]">
+        <Post/>
+    </h1>;
+  }
+  
---
+++ components/Post.jsx
@@ -0,0 +1,9 @@
+const Post = () => {
+    return ( 
+        <div className=" p-10 m-10  rounded-[12px] bg-[#DDC3C3] h-[140px] ">
+            this is a post
+        </div>
+     );
+}
+ 
+export default Post;
